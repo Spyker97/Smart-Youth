c9412f82343bd93274ce1f31b05d270fc291c151
Ahmed BEN HADID <ahmedbhd97@gmail.com>
2023-10-14 13:10:05 +0100

    sddss

---
+++ SmartYouth/SM-getway/src/main/java/com/smartdev/Config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.smartdev.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+public class SecurityConfig {
+
+
+}
